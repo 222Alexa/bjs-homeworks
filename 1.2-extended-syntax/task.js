1651a9f99b8940e650440d774a499c974c141626
Alexandra Titova <auto-steklo@mail.ru>
2020-10-19 00:07:44 +0300

    форматирование кода

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -11,35 +11,31 @@ function getResult(a, b, c) {
         result.push(-b / (2 * a).toFixed(2));
     }
     else if (discriminant > 0) {
-        result.push(((-b + Math.sqrt(discriminant))/ (2 * a)).toFixed(2));
-        result.push(((-b - Math.sqrt(discriminant))/ (2 * a)).toFixed(2));
+        result.push(((-b + Math.sqrt(discriminant)) / (2 * a)).toFixed(2));
+        result.push(((-b - Math.sqrt(discriminant)) / (2 * a)).toFixed(2));
     }
-    
-
     return result
 }
 
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
     // return averageMark;
-let total = []
-let mediane = (marks.reduce((a,b) => (a + b),0 ))/marks.length;
-if(marks.length === 0){
-  return 0
-};
-for (let i = 0; i<= marks.length; i++){
-  if(marks.length < 5) {
-    total = marks;
-
-  }else {
-    total = marks.splice(0,5)
-    mediane = (total.reduce((a,b) => (a + b)))/total.length;
-
-  }return mediane
-  
+    let total = []
+    let mediane = (marks.reduce((a, b) => (a + b), 0)) / marks.length;
+    if (marks.length === 0) {
+        return 0
+    };
+    for (let i = 0; i <= marks.length; i++) {
+        if (marks.length < 5) {
+            total = marks;
+        }
+        else {
+            total = marks.splice(0, 5)
+            mediane = (total.reduce((a, b) => (a + b))) / total.length;
+        }
+        return mediane
+    }
 }
-}
-
 
 function askDrink(name, dateOfBirthday) {
     // код для задачи №3 писать здесь
