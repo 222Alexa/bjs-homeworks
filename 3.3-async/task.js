9dcf05165d3852891bd6f550021698dccb6f0067
Alexandra Titova <auto-steklo@mail.ru>
2020-11-14 16:44:12 +0300

    update

--- 3.3-async/task.js
+++ 3.3-async/task.js
@@ -6,30 +6,30 @@ class AlarmClock {
     }
     addClock(time, callback, id) {
         if (!id) {
-            throw new Error('error text');// id не передан, выброс ошибки
+            throw new Error('error text'); // id не передан, выброс ошибки
         }
         if (this.alarmCollection.some((item) => item.id === id)) {
             return console.error('Будильник уже существует'); //id совпадает с существующим в коллекции
         }
         this.alarmCollection.push({
-            id,
-            time,
-            callback
-        });// добавление уникального будильника в коллекцию
+            id
+            , time
+            , callback
+        }); // добавление уникального будильника в коллекцию
     }
     removeClock(id) {
-        let findId = this.alarmCollection.filter((item) => item.id == id); // поиск в коллекции совпадений с id
-        if (findId) {
-            this.alarmCollection.splice(findId, 1);
-            return true; //нашел - удали
+        let findId = this.alarmCollection.findIndex((item) => item.id == id); // поиск в коллекции совпадений с id
+        if (findId === -1) {// в этом случае условием будет ненайденный объект. 
+            return false;
         }
-        return false;
+        this.alarmCollection.splice(findId, 1);
+        return true; //нашел - удали
     }
     getCurrentFormattedTime() {
         return new Date().toTimeString().slice(0, 5);
     }
     start() {
-        const checkTime = this.getCurrentFormattedTime();
+        const checkTime = this.getCurrentFormattedTime;
 
         function checkClock(clock) {
             const newTime = checkTime();
@@ -39,7 +39,7 @@ class AlarmClock {
         }
         if (this.timerId === null) {
             this.timerId = setInterval(() => {
-                this.alarmCollection.forEach(item => this.checkClock(item)) // перебирает коллекцию и для каждого будильника вызывает checkClock, который вызывает callback, и сохраняет результат в текущем таймере id
+                this.alarmCollection.forEach(item => checkClock(item)) // перебирает коллекцию и для каждого будильника вызывает checkClock, который вызывает callback, и сохраняет результат в текущем таймере id
             }, 60000)
         }
     }
@@ -54,7 +54,7 @@ class AlarmClock {
     }
     clearAlarms() {
         clearInterval(this.timerId);
-        this.alarmCollection.splice(0, this.alarmCollection.length);// очистить все, массив оставить тот же
+        this.alarmCollection.splice(0, this.alarmCollection.length); // очистить все, массив оставить тот же
     }
 }
 const ticTac = new AlarmClock();
